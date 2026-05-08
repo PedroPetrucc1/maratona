83c03423a58061a029cc10d8208d36f116a724bb
Pedro Petrucci <pedropetruccif@gmail.com>
2026-05-08 19:02:16 -0300

    Exercicio feito

---
+++ cses/dp/CoinCombinationsII.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long long MOD = 1e9+7; 
+
+int main(){
+	int n,x; cin >> n >> x;
+	vector <long long> coins(n);
+	long long dp[1000001] = {0};
+	for(int i=0; i < n; i++)cin >> coins[i];
+	
+	dp[0] = 1; //caso base
+	
+	for(int i=0; i < n; i++){
+		for(int j=0; j <= x; j++){
+			if(j >= coins[i]){
+				dp[j] = (dp[j] + dp[j - coins[i]])%MOD;
+			}
+		}
+	}
+	
+	cout << dp[x];
+}
