6cb8d69bff9ccd1e4459172730c2764dee4c8785
Pedro Petrucci <pedropetruccif@gmail.com>
2026-06-22 13:18:00 -0300

    C

---
+++ paulista2026/C.cpp
@@ -0,0 +1,14 @@
+/*
+ *	NOME: World capital of pizza
+ *	LINK: https://codeforces.com/gym/106598/problem/C
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int n, m; cin >> n >> m;
+	cout << (n*8)/m << endl;
+	return 0;
+}
