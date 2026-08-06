46b020f9caaad3d3da08576e59d138e95858c0f2
Pedro Petrucci <pedropetruccif@gmail.com>
2026-08-05 21:37:19 -0300

    F

---
+++ sbc/sbc2023fase1/F.cpp
@@ -0,0 +1,33 @@
+/*
+ *	NOME: 
+ *	LINK: 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n, c, r; cin >> n >> c >> r;
+    vector <ll> cans(c);
+    ll total = r;
+    for(auto &v : cans) cin >> v;
+    
+    while(r--){
+    	ll a; cin >> a;
+    	n += a;
+    }
+    
+    for(ll k : cans){
+    	n -= k;
+    	if(n < 0) break;
+    	total++;
+    }
+    
+    cout << total << '\n';
+    
+	return 0;
+}
