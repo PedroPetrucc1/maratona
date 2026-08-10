4cd3b55a8672ec6654e070aaf6355113eca49d72
Pedro Petrucci <pedropetruccif@gmail.com>
2026-08-10 17:03:23 -0300

    Exercicio feito

---
+++ cses/mathematics/FibonacciNumbers.cpp
@@ -0,0 +1,35 @@
+/*
+ *	NOME: Fibonacci Numbers
+ *	LINK: https://cses.fi/problemset/task/1722
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+ll MOD = 1e9+7;
+
+map <ll, ll> f;
+
+vector <ll> fib(ll n){
+	if(n==0) return {0, 1};
+	
+	vector <ll> res = fib(n/2);	
+	
+	ll fk = (res[0]*((2*res[1] - res[0] + MOD)%MOD))%MOD;
+	ll fk1 = ((res[0]*res[0])%MOD + (res[1]*res[1])%MOD)%MOD;
+	
+	if(n%2 == 0) return {fk, fk1};
+	else return {fk1, (fk+fk1)%MOD};
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	ll n; cin >> n;
+	vector <ll> ans = fib(n);
+	cout << ans[0] << '\n';
+}
